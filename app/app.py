b934d332a0300e3c6b243d76434c9c7a2df98e07
lkc9015 <kl2830@nyu.edu>
2017-07-31 16:55:28 -0400

    read csv file

--- app/app.py
+++ app/app.py
@@ -3,3 +3,12 @@ from nltk.tokenize import RegexpTokenizer
 from stop_words import get_stop_words
 from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models, similarities
+
+
+letters = []
+
+csv_file_path = "data\shareholders_letter.csv"
+with open(csv_file_path, "r") as csv_file:
+    reader = csv.reader(csv_file)
+    for row in reader:
+        letters.append(row[3])
