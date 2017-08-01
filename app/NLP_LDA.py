e85c25c76379b7fdc5f987de564075f2b42a5fd7
lkc9015 <kl2830@nyu.edu>
2017-07-31 22:37:25 -0400

    make LDA and NMF file

--- app/NLP_LDA.py
+++ app/NLP_LDA.py
@@ -9,7 +9,6 @@ from gensim import corpora, models, similarities
 
 ### reading csv file
 letters = []
-full_contents = []
 
 csv_file_path = "data\shareholders_letter.csv"
 with open(csv_file_path, "r") as csv_file:
@@ -18,7 +17,6 @@ with open(csv_file_path, "r") as csv_file:
     for row in reader:
         full_contents.append(row)
         letters.append(row[3])
-
 
 #### tokenizing letters' texts and removing stopwords
 texts = []
@@ -44,8 +42,6 @@ for text in texts:
     once_tokens = [word for word in text if word not in tokens_once]
     clean_texts.append(once_tokens)
 
-
-
 # turn our tokenized documents into a id <-> term dictionary
 dictionary = corpora.Dictionary(clean_texts)
 
@@ -54,45 +50,3 @@ dtm = [dictionary.doc2bow(text) for text in clean_texts]
 
 # generate LDA model
 ldamodel = gensim.models.ldamodel.LdaModel(dtm, num_topics=10, id2word = dictionary, passes=20)
-
-
-
-
-### other
-#import numpy as np
-#import sklearn.feature_extraction.text as text
-#
-#vectorizer = text.CountVectorizer(input='letters', stop_words='english', min_df=4)
-#dtm = vectorizer.fit_transform(letters).toarray()
-#vocab = np.array(vectorizer.get_feature_names())
-#
-#from sklearn import decomposition
-#num_topics = 10
-#num_top_words = 15
-#clf = decomposition.NMF(n_components=num_topics, random_state=1)
-#
-#doctopic = clf.fit_transform(dtm)
-#
-#topic_words = []
-#for topic in clf.components_:
-#    word_idx = np.argsort(topic)[::-1][0:num_top_words]
-#    topic_words.append([vocab[i] for i in word_idx])
-#
-#doctopic = doctopic / np.sum(doctopic, axis=1, keepdims=True)
-#
-#company_names = []
-#for row in full_contents:
-#    company_names.append(row[1])
-#
-#company_names = np.asarray(company_names)
-#doctopic_orig = doctopic.copy()
-#num_companies = len(set(company_names))
-#
-#doctopic_grouped = np.zeros((num_companies, num_topics))
-#
-#for i, name in enumerate(sorted(set(company_names))):
-#    doctopic_grouped[i, :] = np.mean(doctopic[company_names == name, :], axis=0)
-#
-#doctopic = doctopic_grouped
-#
-#print (doctopic)
---
+++ app/NLP_NMF.py
@@ -0,0 +1,48 @@
+import csv
+import numpy as np
+import sklearn.feature_extraction.text as text
+from sklearn import decomposition
+
+## Read CSV file
+letters = []
+
+csv_file_path = "data\shareholders_letter.csv"
+with open(csv_file_path, "r") as csv_file:
+    reader = csv.reader(csv_file)
+    next(reader)
+    for row in reader:
+        letters.append(row[3])
+
+
+#### Change the texts into Documents Term Matix (dtm) ####
+# remove stopwords and words that appear less than ten times
+vectorizer = text.CountVectorizer(input='letters', stop_words='english', min_df=10)
+# create dtm and convert it into an array
+dtm = vectorizer.fit_transform(letters).toarray()
+# list of words and change it to an array
+vocab = np.array(vectorizer.get_feature_names())
+
+### Ten topics & Ten top words in each topic ###
+num_topics = 10
+num_top_words = 10
+# classifier
+clf = decomposition.NMF(n_components=num_topics, random_state=1)
+doctopic = clf.fit_transform(dtm)
+
+
+### Print out topic words ###
+topic_words = []
+for topic in clf.components_:
+    word_idx = np.argsort(topic)[::-1][0:num_top_words]
+    topic_words.append([vocab[i] for i in word_idx])
+
+for topic in range(len(topic_words)):
+    print(" + Topic {}: {}".format(topic, ' '.join(topic_words[topic])))
+
+
+#nmf_path = "data\NLP_NMF_topics.csv"
+#with open(nmf_path, "w") as csv_file:
+#    writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle", "department", "price"])
+#    writer.writeheader()
+#    for product in products:
+#        writer.writerow(product)
